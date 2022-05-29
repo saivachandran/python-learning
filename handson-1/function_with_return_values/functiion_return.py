67dc79b16b7ac37f2802b34ecd837ec305cfba14
saivachandran <chandransaiva@gmail.com>
2022-05-29 12:20:16 +0530

    fnreturn

--- handson-1/function_with_return_values/functiion_return.py
+++ handson-1/function_with_return_values/functiion_return.py
@@ -6,6 +6,11 @@ def days_to_units(num_of_days):
 
     return f"{num_of_days} days are  { num_of_days * calculation_to_units } {name_of_units} "
     
-my_var = days_to_units(30)
+user_input = input("Hey user Enter number of days I will concert into days!\n")   
 
-print(my_var)
+user_input_number = int(user_input)
+
+calculated_value = days_to_units(user_input_number)
+
+print(calculated_value)
+
